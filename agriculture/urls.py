eea749947f9b53276eefd5e19d1e139ac2387592
Limbureuben <limbureuben4@gmail.com>
2025-04-08 12:27:41 +0300

    changes

--- agriculture/urls.py
+++ agriculture/urls.py
@@ -20,8 +20,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('myapp.urls')),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
