28b8ee9138b682f196003d8d985104860e5ef9fd
Limbureuben <limbureuben4@gmail.com>
2025-04-08 13:18:33 +0300

    changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
     path('farm-report/', FarmReportView.as_view(), name='farm-report')
 ]
